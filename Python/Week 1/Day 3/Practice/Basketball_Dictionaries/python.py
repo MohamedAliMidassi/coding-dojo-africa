9fc55a55b01d4eb7ad8b84b6b9da10b42a4f430c
MohamedAliMidassi <mohamedalimidassi@gmail.com>
2024-02-25 22:11:08 +0100

    a

--- Python/Week 1/Day 3/Practice/Basketball_Dictionaries/python.py
+++ Python/Week 1/Day 3/Practice/Basketball_Dictionaries/python.py
@@ -1,47 +1,37 @@
-players = [
-    {
-    	"name": "Kevin Durant", 
-    	"age":34, 
-    	"position": "small forward", 
-    	"team": "Brooklyn Nets"
-    },
-    {
-    	"name": "Jason Tatum", 
-    	"age":24, 
-    	"position": "small forward", 
-    	"team": "Boston Celtics"
-    },
-    {
-    	"name": "Kyrie Irving", 
-    	"age":32,
-        "position": "Point Guard", 
-    	"team": "Brooklyn Nets"
-    },
-    {
-    	"name": "Damian Lillard", 
-    	"age":33,
-        "position": "Point Guard", 
-    	"team": "Portland Trailblazers"
-    },
-    {
-    	"name": "Joel Embiid", 
-    	"age":32,
-        "position": "Power Foward", 
-    	"team": "Philidelphia 76ers"
-    },
-    {
-        "name": "DeMar DeRozan",
-        "age": 32,
-        "position": "Shooting Guard",
-        "team": "Chicago Bulls"
-    }
-]
-class Player:
-    def __init__(self, name, age, position, team):
-        self.name = name
-        self.age = age
-        self.position = position
-        self.team = team
 
-#player=Player(players['name'])
-print(players["name"])
+class Player:
+    def __init__(self, data):
+        self.name = data['name']
+        self.age = data['age']
+        self.position = data['position']
+        self.team = data['team']
+    
+
+
+
+
+kevin = {
+        "name": "Kevin Durant", 
+        "age":34, 
+        "position": "small forward", 
+        "team": "Brooklyn Nets"
+}
+jason = {
+        "name": "Jason Tatum", 
+        "age":24, 
+        "position": "small forward", 
+        "team": "Boston Celtics"
+}
+kyrie = {
+        "name": "Kyrie Irving", 
+        "age":32, "position": "Point Guard", 
+        "team": "Brooklyn Nets"
+}
+    
+
+player_jason = Player(jason)
+player_kevin = Player(kevin)
+player_kyrie = Player(kyrie)
+print(player_jason)
+print(player_kevin)
+print(player_kyrie)

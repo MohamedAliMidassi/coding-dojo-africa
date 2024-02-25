7afdaf35a5a10115531ff1b7fa37b7a73374d763
MohamedAliMidassi <mohamedalimidassi@gmail.com>
2024-02-25 21:30:35 +0100

    update

--- Python/Week 1/Day 2/Core/BankAccount/python.py
+++ Python/Week 1/Day 2/Core/BankAccount/python.py
@@ -1,20 +1,14 @@
 class BankAccount:
-    # don't forget to add some default values for these parameters!
     def __init__(self, int_rate, balance): 
         self.int_rate=int_rate
         self.balance=balance
-        # your code here! (remember, instance attributes go here)
-        # don't worry about user info here; we'll involve the User class soon
     def deposit(self, amount):
-        # your code here
         amount+=5
         print("succssesful deposit")
     def withdraw(self, amount):
-        # your code here
         print("Insuffcient funds: Charging a $5 fee")
         amount-=5
     def display_account_info(self):
-        # your code here
         print("Balance : $100")
     def yield_interest(self):
         # your code here
